262a6c2d6a9221935f60229c5e6a1d3306910b12
Aliyugambo <gamboaliyu00@gmail.com>
2023-09-25 22:11:51 +0100

    ADD:Fullstack-Seazus-Project

--- seazux_frontend/src/Actions/User.actions.js
+++ seazux_frontend/src/Actions/User.actions.js
@@ -1,8 +1,151 @@
+// import axios from 'axios';
+
+// // import { backendUrl } from '../utils/util';
+// // const backendUrl = 'https://shy-teal-worm-coat.cyclic.cloud';
+// // const backendUrl = 'http:${backendUrl}';
+
+// export const registerUserEmail = ({username, email, password}) => async (dispatch) => {
+
+//     try {
+
+//         dispatch({
+//             type: 'RegisterRequest'
+//         })
+//         const {data}  = await axios.post(`https://seazux-backend.onrender.com/auth-email/register`, {username, email, password}, {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             withCredentials: 'true'
+//         })
+//         dispatch({
+//             type: 'RegisterSuccess',
+//             payload: data
+//         })
+//         // console.log(data)
+
+        
+//     } catch (error) {
+//         dispatch({
+//             type: 'RegisterFailure',
+//             payload: error.response.data.message
+//         })
+//     }
+// }
+
+// export const loginUserEmail = ({email, password}) => async (dispatch) => {
+//         try {
+    
+//             dispatch({
+//                 type: 'LoginRequest'
+//             })
+    
+            
+    
+//             const  { data } = await axios.post(`https://seazux-backend.onrender.com/auth-email/login`, {email, password}, {
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//                 withCredentials: 'true'
+//             })
+    
+//             dispatch({
+//                 type: 'LoginSuccess',
+//                 payload: data
+//             })
+//             // console.log(data)
+            
+//         } catch (error) {
+//             dispatch({
+//                 type: 'LoginFailure',
+//                 payload: error.response.data.message
+//             })
+//         }
+// }
+
+// export const loagout = () => async (dispatch) => {
+//     try {
+
+//         dispatch({
+//             type: 'LogoutRequest'
+//         })
+
+
+//         const { data } = await axios.get(`https://seazux-backend.onrender.com/auth-email/logout`, {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             withCredentials: 'true'
+//         })
+
+//         dispatch({
+//             type: 'LogoutSuccess',
+//             payload: data
+//         })
+        
+//     } catch (error) {
+//         dispatch({
+//             type: 'LogoutFailure',
+//             payload: error.response.data.message
+//         })
+//     }
+// }
+
+// export const loadUser = () => async (dispatch) => {
+//     try {
+//         dispatch({
+//             type: 'LoadUserRequest'
+//         })
+
+//         const { data } = await axios.get(`https://seazux-backend.onrender.com/auth-email/user/profile`,{
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             withCredentials: 'true'
+//         })
+
+//         dispatch({
+//             type: 'LoadUserSuccess',
+//             payload: data
+//         })
+        
+//     } catch (error) {
+//         dispatch({
+//             type: 'LoadUserFailure',
+//             // payload: error.response.data.message
+//         })
+//     }
+// }
+
+// export const deleteAccount = () => async (dispatch) => {
+//     try {
+//         dispatch({
+//             type: 'DeleteAccountRequest'
+//         })
+
+//         const  { data } = await axios.delete(`https://seazux-backend.onrender.com/auth-email/user/deleteAccount`, {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             withCredentials: 'true'
+//         })
+
+//         dispatch({
+//             type: 'DeleteAccountSuccess',
+//             payload: data
+//         })
+        
+//     } catch (error) {
+//         dispatch({
+//             type: 'DeleteAccountFailure',
+//             payload: error.response.data.message
+//         })
+//     }
+// }
+
+
 import axios from 'axios';
 
-// import { backendUrl } from '../utils/util';
-// const backendUrl = 'https://shy-teal-worm-coat.cyclic.cloud';
-// const backendUrl = 'http:${backendUrl}';
+import { backendUrl } from '../utils/util';
 
 export const registerUserEmail = ({username, email, password}) => async (dispatch) => {
 
@@ -11,18 +154,21 @@ export const registerUserEmail = ({username, email, password}) => async (dispatc
         dispatch({
             type: 'RegisterRequest'
         })
-        const {data}  = await axios.post(`https://seazux-backend.onrender.com/auth-email/register`, {username, email, password}, {
+
+        const config = {
             headers: {
                 'Content-Type': 'application/json'
             },
             withCredentials: 'true'
-        })
+        }
+
+
+        const { data } = await axios.post(`${backendUrl}/auth-email/register`, {username, email, password}, config);
+
         dispatch({
             type: 'RegisterSuccess',
             payload: data
         })
-        // console.log(data)
-
         
     } catch (error) {
         dispatch({
@@ -39,20 +185,19 @@ export const loginUserEmail = ({email, password}) => async (dispatch) => {
                 type: 'LoginRequest'
             })
     
-            
-    
-            const  { data } = await axios.post(`https://seazux-backend.onrender.com/auth-email/login`, {email, password}, {
+            const config = {
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 withCredentials: 'true'
-            })
+            }
+    
+            const { data } = await axios.post(`${backendUrl}/auth-email/login`, {email, password}, config);
     
             dispatch({
                 type: 'LoginSuccess',
                 payload: data
             })
-            // console.log(data)
             
         } catch (error) {
             dispatch({
@@ -69,13 +214,14 @@ export const loagout = () => async (dispatch) => {
             type: 'LogoutRequest'
         })
 
-
-        const { data } = await axios.get(`https://seazux-backend.onrender.com/auth-email/logout`, {
+        const config = {
             headers: {
                 'Content-Type': 'application/json'
             },
             withCredentials: 'true'
-        })
+        }
+
+        const { data } = await axios.get(`${backendUrl}/auth-email/logout`, config);
 
         dispatch({
             type: 'LogoutSuccess',
@@ -96,12 +242,14 @@ export const loadUser = () => async (dispatch) => {
             type: 'LoadUserRequest'
         })
 
-        const { data } = await axios.get(`https://seazux-backend.onrender.com/auth-email/user/profile`,{
+        const config = {
             headers: {
                 'Content-Type': 'application/json'
             },
             withCredentials: 'true'
-        })
+        }
+
+        const { data } = await axios.get(`${backendUrl}/auth-email/user/profile`, config);
 
         dispatch({
             type: 'LoadUserSuccess',
@@ -122,12 +270,14 @@ export const deleteAccount = () => async (dispatch) => {
             type: 'DeleteAccountRequest'
         })
 
-        const  { data } = await axios.delete(`https://seazux-backend.onrender.com/auth-email/user/deleteAccount`, {
+        const config = {
             headers: {
                 'Content-Type': 'application/json'
             },
             withCredentials: 'true'
-        })
+        }
+
+        const { data } = await axios.delete(`${backendUrl}/auth-email/user/deleteAccount`, config);
 
         dispatch({
             type: 'DeleteAccountSuccess',
