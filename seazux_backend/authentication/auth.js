2f5dd68ccc89ad9f02d63b6b14407af56df83461
Aliyugambo <gamboaliyu00@gmail.com>
2023-09-26 14:28:06 +0100

    ADD:Fullstack-Seazus-Project

--- seazux_backend/authentication/auth.js
+++ seazux_backend/authentication/auth.js
@@ -6,8 +6,8 @@ exports.isAuthenticated = async (req, res, next) => {
     try {
 
         console.log(req.headers);
-        const {token}  = req.cookies;
-        // const {token} = req.headers.authorization.split(" ")[1] || req.cookies.token;
+        // const {token}  = req.cookies;
+        const {token} = req.headers.authorization.split(" ")[1] || req.cookies;
         console.log(token)
         if (!token) {
             return res.status(401).json({
