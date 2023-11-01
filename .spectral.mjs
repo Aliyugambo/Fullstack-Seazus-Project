db399d0c861209a31324083e033ecd05d280016f
gamboaliyu00@gmail.com <gamboaliyu00@gmail.com>
2023-11-01 11:33:24 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyNDkzOQ/spectral.js?branch_slug=master&token=987c3eca-fa00-4742-9ae0-e94d24981878";
+export default { extends: ruleset };
